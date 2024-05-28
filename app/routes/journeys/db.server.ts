a62e1c15b7b21ffa055b87c8c2689ebc233b7d5d
Kent Ntwari <designerjehovah@gmail.com>
2024-05-27 21:32:20 -0400

    feat:init journeys server files

---
+++ app/routes/journeys/db.server.ts
@@ -0,0 +1,77 @@
+import prisma from "~/utils/prisma";
+
+export async function getUserJourneys(userEmail: string) {
+  const data = await prisma.user.findFirst({
+    where: {
+      email: userEmail,
+    },
+    select: {
+      journeys: {
+        select: {
+          id: true,
+          title: true,
+          checkpoints: true,
+          updatedAt: true,
+        },
+      },
+    },
+  });
+
+  await prisma.$disconnect();
+
+  return data;
+}
+
+export async function createUserJourney(
+  user: {
+    email: string;
+    firstName: string;
+    lastName: string;
+  },
+  title: string
+) {
+  // check if user is registered in db
+  const dbUser = await prisma.user.findUnique({
+    where: {
+      email: user.email,
+    },
+  });
+
+  if (!dbUser) {
+    await prisma.user.upsert({
+      where: {
+        email: user.email,
+      },
+      update: {},
+      create: {
+        email: user.email,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        journeys: {
+          create: {
+            title,
+          },
+        },
+      },
+    });
+
+    await prisma.$disconnect();
+    return;
+  }
+
+  await prisma.user.update({
+    where: {
+      email: user.email,
+    },
+    data: {
+      journeys: {
+        create: {
+          title,
+        },
+      },
+    },
+  });
+
+  await prisma.$disconnect();
+  return;
+}
