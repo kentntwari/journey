9b2c54a88b76864d622e0774c041ee4fe55aaf43
Kent Ntwari <designerjehovah@gmail.com>
2024-06-19 13:16:28 -0400

    refactor:

--- app/types.ts
+++ app/types.ts
@@ -1,5 +1,8 @@
 import { z } from "zod";
 
-import { milestoneSchema } from "./utils/schemas";
+import { checkpointSchema, milestoneSchema } from "./utils/schemas";
 
 export type MileStoneEntry = z.infer<typeof milestoneSchema>;
+
+const SkimmedCheckpointSchema = checkpointSchema.omit({ journeyTitle: true });
+export type Checkpoint = z.infer<typeof SkimmedCheckpointSchema>;
