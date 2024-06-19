64dadd2592878441e9fa3f5cf3bfab04131b51dd
Kent Ntwari <designerjehovah@gmail.com>
2024-06-19 13:15:47 -0400

    feat:init new schema

--- app/utils/schemas.ts
+++ app/utils/schemas.ts
@@ -11,10 +11,12 @@ export const milestoneSchema = z.object({
 });
 
 export const challengeSchema = z.object({
+  id: z.string(),
   description: z.string().min(1).max(100),
 });
 
 export const failureSchema = z.object({
+  id: z.string(),
   description: z.string().min(1).max(100),
 });
 
@@ -31,4 +33,41 @@ export const checkpointSchema = z.object({
     .array(),
   challenges: z.array(challengeSchema),
   failures: z.array(failureSchema),
+});
+
+export const skimmedCheckpointSchema = checkpointSchema
+  .omit({
+    journeyTitle: true,
+    description: true,
+  })
+  .extend({
+    id: z.string(),
+  })
+  .transform((data) => ({
+    ...data,
+    milestones: data.milestones.map((milestone) => ({
+      id: milestone.id,
+    })),
+    challenges: data.challenges.map((challenge) => ({
+      id: challenge.id,
+    })),
+    failures: data.failures.map((failure) => ({
+      id: failure.id,
+    })),
+  }));
+
+export const journeySchema = z.object({
+  id: z.string(),
+  title: z.string(),
+  updatedAt: z.date(),
+  checkpoints: z.array(
+    z.object({
+      id: z.string(),
+      title: z.string(),
+    })
+  ),
+});
+
+export const newJourneyschema = z.object({
+  title: z.string().min(1).max(100),
 });
