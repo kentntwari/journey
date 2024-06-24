cb8cae5339744a90a97c9fccbf7b066f775c134b
Kent Ntwari <designerjehovah@gmail.com>
2024-06-24 11:57:09 -0400

    feat:add checkpoint details types

--- app/types.ts
+++ app/types.ts
@@ -1,8 +1,15 @@
 import { z } from "zod";
 
+import { getDetails as getCheckpointDetails } from "~/routes/ressource.checkpoint.$id/db.server";
+
 import { checkpointSchema, milestoneSchema } from "./utils/schemas";
 
 export type MileStoneEntry = z.infer<typeof milestoneSchema>;
 
 const SkimmedCheckpointSchema = checkpointSchema.omit({ journeyTitle: true });
 export type Checkpoint = z.infer<typeof SkimmedCheckpointSchema>;
+
+export type SingleCheckpointFetchedData = {
+  results: Awaited<ReturnType<typeof getCheckpointDetails>> | null;
+  error: boolean | undefined;
+};
