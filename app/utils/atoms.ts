b85a19b58c765fefbc4b5be199f9814ce059dee8
Kent Ntwari <designerjehovah@gmail.com>
2024-06-25 06:25:42 -0400

    feat:add challenges and failures

--- app/utils/atoms.ts
+++ app/utils/atoms.ts
@@ -1,4 +1,4 @@
-import type { MileStoneEntry } from "~/types";
+import type { MileStoneEntry, ChallengeEntry, FailureEntry } from "~/types";
 
 import { atom } from "jotai";
 
@@ -6,4 +6,12 @@ export const isDialogOpenAtom = atom(false);
 
 export const isAddMilestoneAtom = atom(false);
 
+export const isAddChallengeAtom = atom(false);
+
+export const isAddFailureAtom = atom(false);
+
 export const pendingMilestonesAtom = atom<MileStoneEntry[]>([]);
+
+export const pendingChallengesAtom = atom<ChallengeEntry[]>([]);
+
+export const pendingFailuresAtom = atom<FailureEntry[]>([]);
