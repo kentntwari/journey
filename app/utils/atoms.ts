64be060d549222b2c43a61088577d640bbda5217
Kent Ntwari <designerjehovah@gmail.com>
2024-06-27 11:57:33 -0400

    chore: remove breaking lines

--- app/utils/atoms.ts
+++ app/utils/atoms.ts
@@ -5,13 +5,9 @@ import { atom } from "jotai";
 export const isDialogOpenAtom = atom(false);
 
 export const isAddMilestoneAtom = atom(false);
-
 export const isAddChallengeAtom = atom(false);
-
 export const isAddFailureAtom = atom(false);
 
 export const pendingMilestonesAtom = atom<MileStoneEntry[]>([]);
-
 export const pendingChallengesAtom = atom<ChallengeEntry[]>([]);
-
 export const pendingFailuresAtom = atom<FailureEntry[]>([]);
