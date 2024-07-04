0774115df10145ef160898ab44cea8859c85a34e
Kent Ntwari <designerjehovah@gmail.com>
2024-07-04 19:32:58 -0400

    feat: new atoms

--- app/utils/atoms.ts
+++ app/utils/atoms.ts
@@ -3,6 +3,7 @@ import type { MileStoneEntry, ChallengeEntry, FailureEntry } from "~/types";
 import { atom } from "jotai";
 
 export const isDialogOpenAtom = atom(false);
+export const isAlertDialogOpenAtom = atom(false);
 
 export const isAddMilestoneAtom = atom(false);
 export const isAddChallengeAtom = atom(false);
@@ -11,3 +12,8 @@ export const isAddFailureAtom = atom(false);
 export const pendingMilestonesAtom = atom<MileStoneEntry[]>([]);
 export const pendingChallengesAtom = atom<ChallengeEntry[]>([]);
 export const pendingFailuresAtom = atom<FailureEntry[]>([]);
+
+export const isEditCheckpointStartDateAtom = atom(false);
+export const isEditCheckpointTitleAtom = atom(false);
+export const isEditCheckpointDescriptionAtom = atom(false);
+export const isDeleteCheckpointAtom = atom(false);
