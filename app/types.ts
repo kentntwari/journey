1d4772c23968033d0279f3fb0a73b6e9dc4508f8
Kent Ntwari <designerjehovah@gmail.com>
2024-07-04 19:30:09 -0400

    feat: add new types

--- app/types.ts
+++ app/types.ts
@@ -3,21 +3,26 @@ import { z } from "zod";
 import { getDetails as getCheckpointDetails } from "~/routes/ressource.checkpoint.$id/db.server";
 
 import {
+  checkpointTitleSchema,
+  checkpointDescriptionSchema,
+  checkpointStartDateSchema,
+  deleteCheckpointSchema,
   checkpointSchema,
   milestoneSchema,
   challengeSchema,
   failureSchema,
+  newJourneyschema,
 } from "./utils/schemas";
 
-const SkimmedCheckpointSchema = checkpointSchema.omit({ journeyTitle: true });
-
+export type TitleEntry = z.infer<typeof checkpointTitleSchema>;
+export type DescriptionEntry = z.infer<typeof checkpointDescriptionSchema>;
+export type StartDateEntry = z.infer<typeof checkpointStartDateSchema>;
 export type MileStoneEntry = z.infer<typeof milestoneSchema>;
-
 export type ChallengeEntry = z.infer<typeof challengeSchema>;
-
 export type FailureEntry = z.infer<typeof failureSchema>;
-
-export type Checkpoint = z.infer<typeof SkimmedCheckpointSchema>;
+export type CheckpointEntry = z.infer<typeof checkpointSchema>;
+export type JourneyEntry = z.infer<typeof newJourneyschema>;
+export type DeleteCheckpointEntry = z.infer<typeof deleteCheckpointSchema>;
 
 export type SingleCheckpointFetchedData = {
   results: Awaited<ReturnType<typeof getCheckpointDetails>> | null;
