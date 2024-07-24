5b82f5e00ed523b85ce8478a102bb4a123a8ca30
Kent Ntwari <designerjehovah@gmail.com>
2024-07-23 22:18:39 -0400

    fix:fix import path

--- app/routes/ressource.form.checkpoint/Tabs.tsx
+++ app/routes/ressource.form.checkpoint/Tabs.tsx
@@ -7,7 +7,7 @@ import * as UITabs from "~/components/ui/tabs";
 
 import { useCurrentCheckpointDetails } from "~/hooks/useCurrentCheckpointDetails";
 
-// import { Milestones } from "./Milestones";
+import Milestones from "./Milestones";
 import { Challenges } from "./Challenges";
 import { Failures } from "./Failures";
 
@@ -90,10 +90,9 @@ export function Tabs({
             <>
               {typeof initialValues !== "undefined" && (
                 <>
-                Milestones
-                  {/* {tab === "milestones" ? (
+                  {tab === "milestones" ? (
                     <Milestones initialValues={initialValues.milestones} />
-                  ) : null} */}
+                  ) : null}
 
                   {tab === "challenges" ? (
                     <Challenges initialValues={initialValues.challenges} />
